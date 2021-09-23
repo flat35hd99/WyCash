17c7ebf5028ac0e1063644cd694bc4de77bda607
Wataru Sugiura <38929316+flat35hd99@users.noreply.github.com>
2021-09-23 17:08:21 +0000

    1-4

---
+++ src/test/java/com/mycompany/money/MoneyTest.java
@@ -0,0 +1,20 @@
+package com.mycompany.money;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class MoneyTest
+{
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+    }
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+}
